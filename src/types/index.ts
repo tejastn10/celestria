c58b9e387dfb210fa5250ff9c7947f744f3f538d
Tejas Nikhar <tejastn10@gmail.com>
2025-03-11 00:07:41 +0530

    feat: add new types for editor settings and available options

---
+++ src/types/index.ts
@@ -0,0 +1,54 @@
+type EditorSettings = {
+	code: string;
+	language: string;
+
+	theme: string;
+	background: string;
+	backgroundType: "solid" | "gradient";
+	gradientDirection: string;
+
+	padding: number;
+
+	fontFamily: string;
+	fontSize: number;
+
+	lineNumbers: boolean;
+
+	windowControls: boolean;
+
+	watermark: boolean;
+};
+
+type AvailableFont = {
+	id: string;
+	name: string;
+};
+
+type AvailableTheme = {
+	id: string;
+	name: string;
+};
+
+type AvailableLanguage = {
+	id: string;
+	name: string;
+};
+
+type AvailableBackground = {
+	id: string;
+	name: string;
+	value: string;
+
+	type: "solid" | "gradient";
+	gradientDirection?: string;
+};
+
+export type {
+	EditorSettings,
+
+	// ? Actual Editor Settings
+	AvailableFont,
+	AvailableTheme,
+	AvailableLanguage,
+	AvailableBackground,
+};
